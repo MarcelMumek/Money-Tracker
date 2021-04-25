779f6a7494c9e8f8d974f397837d614f2e925ded
Marcel Mumek <s21810156@student.unklab.ac.id>
2021-04-25 19:29:58 +0800

    Exercise 8

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {SplashScreen, SignIn} from './pages';
+import {NavigationContainer } from '@react-navigation/native';
+import Router from './router';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Router />
+    </NavigationContainer>
+  );
+};
+
+export default App;
